0b245370ba5ab1898a4848e48b4bf9dc7934d67d
Miguel Angel Suevis Pacheco <miguelangelsuevispacheco@Miguels-MacBook-Pro.local>
2022-09-30 17:11:38 -0400

    add partial to project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'home' });
+});
+
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('pages/about', { title: 'about' });
+});
+
+
+/* GET project page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'projects' });
+});
+
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'services' });
+});
+
+
+/* GET contact me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'contact me' });
+});
+
 
 module.exports = router;
