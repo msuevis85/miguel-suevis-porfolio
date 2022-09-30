0b99856add18ea0e52a73a6d0e9ae9a9b5f9fa4c
Miguel Angel Suevis Pacheco <miguelangelsuevispacheco@Miguels-MacBook-Pro.local>
2022-09-29 22:30:30 -0400

    restrcuture folder structure

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('miguel-suevis-porfolio:server');
 var http = require('http');
 
